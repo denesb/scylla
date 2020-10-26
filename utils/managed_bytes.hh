264599df4a9393b2feeb0080a05ff4fcaa1830fc
Benny Halevy <bhalevy@scylladb.com>
2020-10-27 01:59:20 +0200

    managed_bytes: implement hash
    For now, implement only hash for managed_bytes_view
    and convert managed_bytes to managed_bytes_view
    to calculate its hash.
    
    Linearize the bytes first for now, for simplicity
    and copleteness, despite being ineficient for
    multiple segments.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- utils/managed_bytes.hh
+++ utils/managed_bytes.hh
@@ -481,6 +481,13 @@ public:
         return fragment_iterator();
     }
 
+    size_t hash() const noexcept {
+        // FIXME: calculate hash inefficiently for now
+        return with_linearized([this] (bytes_view v) {
+            return std::hash<bytes_view>{}(v);
+        });
+    }
+
 private:
     void do_linearize_pure(bytes_view::value_type*) const noexcept;
 
@@ -583,10 +590,16 @@ int compare_unsigned(const managed_bytes_view v1, const managed_bytes_view v2);
 namespace std {
 
 template <>
+struct hash<managed_bytes_view> {
+    size_t operator()(managed_bytes_view v) const {
+        return v.hash();
+    }
+};
+
+template <>
 struct hash<managed_bytes> {
     size_t operator()(const managed_bytes& v) const {
-        return 3; // FIXME!
-        //return hash<bytes_view>()(v);
+        return hash<managed_bytes_view>{}(v);
     }
 };
 
