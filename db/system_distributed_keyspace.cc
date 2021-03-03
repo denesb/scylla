d35333d49d2d908bc18b334ab147283b49916f01
Calle Wilund <calle@scylladb.com>
2021-03-03 15:41:42 +0000

    system_distributed_keyspace: Fix cdc_get_versioned_streams timestamp range
    With the new scheme for cdc generation management, one of the last
    changes was to make the time ordering of the stream timestamps reversed.
    
    However, cdc_get_versioned_streams forgot to take this into account
    when sifting out timestamp ranges for stream retrieval (based on
    low mark).
    
    Fixed by doing reverse iteration.
    
    Refs: #8209.
    Fixes: #8210.
    
    (cherry picked from commit 5a69250d7e3ad4af4160aa5aeaf23a951a8f6d09)

--- db/system_distributed_keyspace.cc
+++ db/system_distributed_keyspace.cc
@@ -524,13 +524,13 @@ system_distributed_keyspace::cdc_get_versioned_streams(db_clock::time_point not_
             }
 
             // `time` is the table's clustering key, so the results are already sorted
-            auto first = std::lower_bound(timestamps.begin(), timestamps.end(), not_older_than);
+            auto first = std::lower_bound(timestamps.rbegin(), timestamps.rend(), not_older_than);
             // need first gen _intersecting_ the timestamp.
-            if (first != timestamps.begin()) {
+            if (first != timestamps.rbegin()) {
                 --first;
             }
 
-            return max_concurrent_for_each(first, timestamps.end(), 5, [this, &ctx, &result] (db_clock::time_point ts) {
+            return max_concurrent_for_each(first, timestamps.rend(), 5, [this, &ctx, &result] (db_clock::time_point ts) {
                 return _qp.execute_internal(
                         format("SELECT streams FROM {}.{} WHERE time = ?", NAME, CDC_DESC_V2),
                         quorum_if_many(ctx.num_token_owners),
