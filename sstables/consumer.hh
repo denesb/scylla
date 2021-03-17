2d4d6f79bc83a0147759f10952b0f23ed24bc183
Botond Dénes <bdenes@scylladb.com>
2021-03-17 09:47:14 +0200

    sstables: continuous_data_consumer: mark permit as blocked when doing IO

--- sstables/consumer.hh
+++ sstables/consumer.hh
@@ -398,6 +398,7 @@ protected:
     sstables::reader_position_tracker _stream_position;
     // remaining length of input to read (if <0, continue until end of file).
     uint64_t _remain;
+    std::optional<reader_permit::blocked_guard> _blocked_guard;
 public:
     using read_status = data_consumer::read_status;
 
@@ -413,6 +414,14 @@ public:
 
     void verify_end_state() {
         state_processor().verify_end_state();
+    }
+
+    void mark_blocked() {
+        _blocked_guard.emplace(_permit);
+    }
+
+    void mark_unblocked() {
+        _blocked_guard.reset();
     }
 
     data_consumer::processing_result skip(temporary_buffer<char>& data, uint32_t len) {
@@ -455,6 +464,7 @@ public:
     // called by input_stream::consume():
     future<consumption_result_type>
     operator()(temporary_buffer<char> data) {
+        mark_unblocked();
         if (data.size() >= _remain) {
             // We received more data than we actually care about, so process
             // the beginning of the buffer, and return the rest to the stream
@@ -482,6 +492,7 @@ public:
                 _remain -= orig_data_size - data.size();
                 _stream_position.position -= data.size();
                 if (value == proceed::yes) {
+                    mark_blocked();
                     return make_ready_future<consumption_result_type>(continue_consuming{});
                 } else {
                     return make_ready_future<consumption_result_type>(stop_consuming<char>{std::move(data)});
@@ -499,6 +510,7 @@ public:
                 }
                 _stream_position.position += skip.get_value();
                 _remain -= skip.get_value();
+                mark_blocked();
                 return make_ready_future<consumption_result_type>(std::move(skip));
             });
         }
@@ -513,7 +525,8 @@ public:
         _remain = end - _stream_position.position;
 
         primitive_consumer::reset();
-        return _input.skip(n);
+        reader_permit::blocked_guard _{_permit};
+        co_await _input.skip(n);
     }
 
     future<> skip_to(size_t begin) {
