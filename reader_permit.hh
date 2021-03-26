ce071354e420e621d62ba6b3d5864963aa15ad7b
Botond Dénes <bdenes@scylladb.com>
2021-03-26 15:44:33 +0200

    reader_permit: prepare for up-front admission
    We want to make permits be admitted up-front, before even being created.
    As part of this change, we will get rid of the `wait_admission()`
    method on the permit, instead, the permit will be created as a result of
    waiting for admission (just like back some time ago).
    To allow evicted readers to wait for re-admission, a new method
    `maybe_wait_readmission()` is created, which waits for readmission if
    the permit is in evicted state.
    The `incorporate()` method is added as glue for the current
    `do_wait_admission()` interface, which is still kept in its current form
    until we make the switch to up-front admission.

--- reader_permit.hh
+++ reader_permit.hh
@@ -124,6 +124,8 @@ private:
 
     void mark_unblocked() noexcept;
 
+    void incorporate(resource_units res);
+
     operator bool() const { return bool(_impl); }
 
     friend class optimized_optional<reader_permit>;
@@ -144,6 +146,8 @@ public:
     reader_concurrency_semaphore& semaphore();
 
     future<resource_units> wait_admission(size_t memory, db::timeout_clock::time_point timeout);
+
+    future<> maybe_wait_readmission(db::timeout_clock::time_point timeout);
 
     void consume(reader_resources res);
 
