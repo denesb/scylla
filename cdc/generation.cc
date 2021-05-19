scylladb/scylladb

cdc: log a message when creating a new CDC generation

cdc/generation.cc
@@ -359,6 +359,8 @@ future<cdc::generation_id> make_new_cdc_generation(
                 2 * ring_delay + duration_cast<milliseconds>(generation_leeway)))};
     co_await sys_dist_ks.insert_cdc_topology_description(gen_id, std::move(gen), { tmptr->count_normal_token_owners() });
 
+    cdc_log.info("New CDC generation: {}", gen_id);
+
     co_return gen_id;
 }
 

denesb/scylla

Refs #8283.
(cherry picked from commit 6525111d213aa8aa07e16a64841de61f77710018)

cdc/generation.cc
@@ -347,6 +347,8 @@ db_clock::time_point make_new_cdc_generation(
                 2 * ring_delay + duration_cast<milliseconds>(generation_leeway)));
     sys_dist_ks.insert_cdc_topology_description(ts, std::move(gen), { tm.count_normal_token_owners() }).get();
 
+    cdc_log.info("New CDC generation: {}", ts);
+
     return ts;
 }
 