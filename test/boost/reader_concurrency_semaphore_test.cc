006ff1d70f9baa5cc2b8480c99b6d0656a702f27
Botond Dénes <bdenes@scylladb.com>
2021-05-20 09:34:46 +0300

    test/boost/reader_concurrency_semaphore_test: add used/blocked test
    Make sure that releasing a bunch of used/blocked guards in random order
    doesn't break the permit state.

--- test/boost/reader_concurrency_semaphore_test.cc
+++ test/boost/reader_concurrency_semaphore_test.cc
@@ -810,3 +810,55 @@ SEASTAR_THREAD_TEST_CASE(test_reader_concurrency_semaphore_admission) {
     BOOST_REQUIRE(semaphore.available_resources() == initial_resources);
     require_can_admit(true, "semaphore in initial state");
 }
+
+SEASTAR_THREAD_TEST_CASE(test_reader_concurrency_semaphore_used_blocked) {
+    const auto initial_resources = reader_concurrency_semaphore::resources{2, 2 * 1024};
+    reader_concurrency_semaphore semaphore(initial_resources.count, initial_resources.memory, get_name());
+    auto stop_sem = deferred_stop(semaphore);
+
+    BOOST_REQUIRE_EQUAL(semaphore.get_permit_stats().total_permits, 0);
+    BOOST_REQUIRE_EQUAL(semaphore.get_permit_stats().used_permits, 0);
+    BOOST_REQUIRE_EQUAL(semaphore.get_permit_stats().blocked_permits, 0);
+
+    auto permit = semaphore.obtain_permit(nullptr, get_name(), 1024, db::no_timeout).get0();
+
+    std::vector<reader_permit::used_guard> used;
+    std::vector<reader_permit::blocked_guard> blocked;
+
+    // The order of the first two is important.
+    used.emplace_back(permit);
+
+    BOOST_REQUIRE_EQUAL(semaphore.get_permit_stats().total_permits, 1);
+    BOOST_REQUIRE_EQUAL(semaphore.get_permit_stats().used_permits, 1);
+    BOOST_REQUIRE_EQUAL(semaphore.get_permit_stats().blocked_permits, 0);
+
+    blocked.emplace_back(permit);
+
+    BOOST_REQUIRE_EQUAL(semaphore.get_permit_stats().total_permits, 1);
+    BOOST_REQUIRE_EQUAL(semaphore.get_permit_stats().used_permits, 1);
+    BOOST_REQUIRE_EQUAL(semaphore.get_permit_stats().blocked_permits, 1);
+
+    for (unsigned i = 0; i < 100; ++i) {
+        if (tests::random::get_bool()) {
+            used.emplace_back(permit);
+        } else {
+            blocked.emplace_back(permit);
+        }
+    }
+
+    while (!used.empty() && !blocked.empty()) {
+        const bool pop_used = !used.empty() && tests::random::get_bool();
+
+        if (pop_used) {
+            used.pop_back();
+            if (used.empty()) {
+                BOOST_REQUIRE_EQUAL(semaphore.get_permit_stats().used_permits, 0);
+            }
+        } else {
+            blocked.pop_back();
+            if (blocked.empty()) {
+                BOOST_REQUIRE_EQUAL(semaphore.get_permit_stats().blocked_permits, 0);
+            }
+        }
+    }
+}
