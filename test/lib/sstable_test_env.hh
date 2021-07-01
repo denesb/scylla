eefde4fd2ce4cd158507660fd3018e724ae1506b
Botond Dénes <bdenes@scylladb.com>
2021-07-01 15:54:43 +0300

    test/lib/sstable_test_env: add reader_concurrency_semaphore member
    To enable tests using the test env to conveniently create permits for
    themselves, reducing the pain of migrating to local semaphores.

--- test/lib/sstable_test_env.hh
+++ test/lib/sstable_test_env.hh
@@ -43,11 +43,17 @@ public:
 
 class test_env {
     std::unique_ptr<test_env_sstables_manager> _mgr;
+    std::unique_ptr<reader_concurrency_semaphore> _semaphore;
 public:
-    explicit test_env() : _mgr(std::make_unique<test_env_sstables_manager>(nop_lp_handler, test_db_config, test_feature_service)) { }
+    explicit test_env()
+        : _mgr(std::make_unique<test_env_sstables_manager>(nop_lp_handler, test_db_config, test_feature_service))
+        , _semaphore(std::make_unique<reader_concurrency_semaphore>(reader_concurrency_semaphore::no_limits{}, "sstables::test_env"))
+    { }
 
     future<> stop() {
-        return _mgr->close();
+        return _mgr->close().finally([this] {
+            return _semaphore->stop();
+        });
     }
 
     shared_sstable make_sstable(schema_ptr schema, sstring dir, unsigned long generation,
@@ -74,6 +80,8 @@ public:
     future<shared_sstable> reusable_sst(schema_ptr schema, sstring dir, unsigned long generation);
 
     test_env_sstables_manager& manager() { return *_mgr; }
+    reader_concurrency_semaphore& semaphore() { return *_semaphore; }
+    reader_permit make_reader_permit(const schema* const s = nullptr, const char* n = "test") { return _semaphore->make_permit(s, n); }
 
     future<> working_sst(schema_ptr schema, sstring dir, unsigned long generation) {
         return reusable_sst(std::move(schema), dir, generation).then([] (auto ptr) { return make_ready_future<>(); });
