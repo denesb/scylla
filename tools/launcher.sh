718c763864681e481b909ed0f9015d1cc9a4079b
Botond Dénes <bdenes@scylladb.com>
2021-11-19 11:07:38 +0200

    tools/launcher.sh: configure seastar to be less intrusive
    The seastar defaults are pretty intrusive for a tool, which should not
    disturb a potentially running scylla process on the machine it is
    invoked on. To this end, this patch adds some command line options which
    with the aim of configuring seastar to be less intrusive -- not act like
    it owns the place.
    This is until we have a C++ API to do the same from the tools
    themselves.

--- tools/launcher.sh
+++ tools/launcher.sh
@@ -3,4 +3,4 @@
 path=$(dirname $0)
 bin=$(basename $0)
 
-exec -a $bin ${path}/../scylla "$@"
+exec -a $bin ${path}/../scylla -c1 -m2G --overprovisioned --blocked-reactor-notify-ms=99999 --relaxed-dma --unsafe-bypass-fsync=1 --mbind=0 --lock-memory=0 --idle-poll-time-us=0 --poll-aio=0 "$@"
