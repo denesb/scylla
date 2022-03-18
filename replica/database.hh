2fd5ff1207bc21377a434260bbd97b3be8ff1bb7
Botond Dénes <bdenes@scylladb.com>
2022-03-18 09:39:19 +0200

    replica: disentangle data_dictionary_impl from database
    Make it a standalone class, instead of private subclass of database.
    Unfriend database and instead make wrap/unwrap methods public, so anyone
    can use them.

--- replica/database.hh
+++ replica/database.hh
@@ -1335,8 +1335,6 @@ private:
     std::unique_ptr<wasm::engine> _wasm_engine;
     utils::cross_shard_barrier _stop_barrier;
 
-    class data_dictionary_impl;
-    friend class data_dictionary_impl;
 public:
     data_dictionary::database as_data_dictionary() const;
     future<> init_commitlog();
