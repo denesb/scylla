a36bc09933e2f402a4cfe888d404c6c36f08f486
Botond Dénes <bdenes@scylladb.com>
2022-04-07 15:16:53 +0300

    readers: extract range tombstone merger into own header

--- readers/combined.cc
+++ readers/combined.cc
@@ -12,6 +12,7 @@
 #include "readers/empty_v2.hh"
 #include "readers/clustering_combined.hh"
 #include "readers/combined.hh"
+#include "readers/range_tombstone_change_merger.hh"
 
 extern logging::logger mrlog;
 
@@ -19,8 +20,6 @@ static constexpr size_t merger_small_vector_size = 4;
 
 template<typename T>
 using merger_vector = utils::small_vector<T, merger_small_vector_size>;
-
-using stream_id_t = const flat_mutation_reader_v2*;
 
 struct mutation_fragment_and_stream_id {
     mutation_fragment_v2 fragment;
@@ -31,82 +30,6 @@ struct mutation_fragment_and_stream_id {
 };
 
 using mutation_fragment_batch = boost::iterator_range<merger_vector<mutation_fragment_and_stream_id>::iterator>;
-
-// Merges range tombstone changes coming from different streams (readers).
-//
-// Add tombstones by calling apply().
-// Updates to the same stream identified by stream_id overwrite each other.
-// Applying an empty tombstone to a stream removes said stream.
-//
-// Call get() to get the tombstone with the highest timestamp from the currently
-// active ones.
-// Get returns disengaged optional when the last apply() call(s) didn't introduce
-// a change in the max tombstone.
-//
-// Call clear() when leaving the range abruptly (next_partition() or
-// fast_forward_to()).
-//
-// The merger doesn't keep track of the position component of the tombstones.
-// Emit them with the current position.
-class range_tombstone_change_merger {
-    struct stream_tombstone {
-        stream_id_t stream_id;
-        ::tombstone tombstone;
-    };
-
-private:
-    std::vector<stream_tombstone> _tombstones;
-    tombstone _current_tombstone;
-
-private:
-    const stream_tombstone* get_tombstone() const {
-        const stream_tombstone* max_tomb{nullptr};
-
-        for (const auto& tomb : _tombstones) {
-            if (!max_tomb || tomb.tombstone > max_tomb->tombstone) {
-                max_tomb = &tomb;
-            }
-        }
-        return max_tomb;
-    }
-
-public:
-    void apply(stream_id_t stream_id, tombstone tomb) {
-        auto it = std::find_if(_tombstones.begin(), _tombstones.end(), [&] (const stream_tombstone& tomb) {
-            return tomb.stream_id == stream_id;
-        });
-        if (it == _tombstones.end()) {
-            if (tomb) {
-                _tombstones.push_back({stream_id, tomb});
-            }
-        } else {
-            if (tomb) {
-                it->tombstone = tomb;
-            } else {
-                auto last = _tombstones.end() - 1;
-                if (it != last) {
-                    std::swap(*it, *last);
-                }
-                _tombstones.pop_back();
-            }
-        }
-    }
-
-    std::optional<tombstone> get() {
-        const auto* tomb = get_tombstone();
-        if (tomb && tomb->tombstone == _current_tombstone) {
-            return {};
-        } else {
-            _current_tombstone = tomb ? tomb->tombstone : tombstone();
-            return _current_tombstone;
-        }
-    }
-
-    void clear() {
-        _tombstones.clear();
-        _current_tombstone = {};
-    }
-};
 
 template<typename Producer>
 concept FragmentProducer = requires(Producer p, dht::partition_range part_range, position_range pos_range) {
