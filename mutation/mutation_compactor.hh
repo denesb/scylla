paszkow/scylladb

compaction_garbage_collector: Extend return type of max_purgeable_fn
Currently, when a max purgeable timestamp is computed, there is no
information where it comes from and how the value was obtained.
Take compaction, if there are memtables or other uncompacting sstables
possibly shadowing data, the timestamp is decreased to ensure a
tombstone is not purged but the caller does not know what that the
timestamp has its value.

In this patch, we extend the return type of max_purgeable_fn to
contain not only a timestamp but also an optional information on
how it was computed. This information will be required to collect
statistics on tombstone purge failures due to overlapping
memtables/uncompacting sstables that come later in the series.

mutation/mutation_compactor.hh
@@ -149,7 +149,7 @@ class compact_mutation_state {
     gc_clock::time_point _query_time;
     max_purgeable_fn _get_max_purgeable;
     can_gc_fn _can_gc;
-    api::timestamp_type _max_purgeable = api::missing_timestamp;
+    max_purgeable _max_purgeable;
     std::optional<gc_clock::time_point> _gc_before;
     const query::partition_slice& _slice;
     uint64_t _row_limit{};
@@ -289,11 +289,11 @@ private:
         if (!t) {
             return false;
         }
-        if (_max_purgeable == api::missing_timestamp) {
+        if (!_max_purgeable) {
             _max_purgeable = _get_max_purgeable(*_dk, is_shadowable);
         }
-        auto ret = t.timestamp < _max_purgeable;
-        mclog.debug("can_gc: t={} is_shadowable={} max_purgeable={}: ret={}", t, is_shadowable, _max_purgeable, ret);
+        auto ret = t.timestamp < _max_purgeable.timestamp;
+        mclog.debug("can_gc: t={} is_shadowable={} max_purgeable={}: ret={}", t, is_shadowable, _max_purgeable.timestamp, ret);
         return ret;
     };
 
@@ -350,7 +350,7 @@ public:
         _static_row_live = false;
         _partition_tombstone = {};
         _current_partition_limit = std::min(_row_limit, _partition_row_limit);
-        _max_purgeable = api::missing_timestamp;
+        _max_purgeable = {};
         _gc_before = std::nullopt;
         _last_static_row.reset();
         _last_pos = position_in_partition::for_partition_start();

denesb/scylla

contain not only a timestamp but also an information on how it was
computed. This information will be required to collect statistics
on tombstone purge failures due to overlapping memtables/uncompacting
sstables that come later in the series.
(cherry picked from commit 503d4f014cea646a4268c93b090832a52afaa963)

mutation/mutation_compactor.hh
@@ -149,8 +149,8 @@ class compact_mutation_state {
     gc_clock::time_point _query_time;
     max_purgeable_fn _get_max_purgeable;
     can_gc_fn _can_gc;
-    api::timestamp_type _max_purgeable_regular = api::missing_timestamp;
-    api::timestamp_type _max_purgeable_shadowable = api::missing_timestamp;
+    max_purgeable _max_purgeable_regular;
+    max_purgeable _max_purgeable_shadowable;
     std::optional<gc_clock::time_point> _gc_before;
     const query::partition_slice& _slice;
     uint64_t _row_limit{};
@@ -290,11 +290,11 @@ private:
             return false;
         }
         auto& max_purgeable = is_shadowable ? _max_purgeable_shadowable : _max_purgeable_regular;
-        if (max_purgeable == api::missing_timestamp) {
+        if (!max_purgeable) {
             max_purgeable = _get_max_purgeable(*_dk, is_shadowable);
         }
-        auto ret = t.timestamp < max_purgeable;
-        mclog.debug("can_gc: t={} is_shadowable={} max_purgeable={}: ret={}", t, is_shadowable, max_purgeable, ret);
+        auto ret = t.timestamp < max_purgeable.timestamp;
+        mclog.debug("can_gc: t={} is_shadowable={} max_purgeable={}: ret={}", t, is_shadowable, max_purgeable.timestamp, ret);
         return ret;
     };
 
@@ -349,8 +349,8 @@ public:
         _static_row_live = false;
         _partition_tombstone = {};
         _current_partition_limit = std::min(_row_limit, _partition_row_limit);
-        _max_purgeable_regular = api::missing_timestamp;
-        _max_purgeable_shadowable = api::missing_timestamp;
+        _max_purgeable_regular = {};
+        _max_purgeable_shadowable = {};
         _gc_before = std::nullopt;
         _last_static_row.reset();
         _last_pos = position_in_partition::for_partition_start();