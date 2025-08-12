scylladb/scylladb

replica/memtable_list: add tombstone_gc_state* member
To be passed down to the memtable.

replica/database.cc
@@ -1946,7 +1946,7 @@ future<> memtable_list::flush() {
 lw_shared_ptr<memtable> memtable_list::new_memtable() {
     return make_lw_shared<memtable>(_current_schema(), *_dirty_memory_manager,
             _table_shared_data,
-            _table_stats, this, _compaction_scheduling_group);
+            _table_stats, this, _compaction_scheduling_group, _gc_state);
 }
 
 // Synchronously swaps the active memtable with a new, empty one,

denesb/scylla

(cherry picked from commit 9d00d7e08dc5be0f785cea4def9acd17521b676f)

replica/database.cc
@@ -1816,7 +1816,7 @@ future<> memtable_list::flush() {
 lw_shared_ptr<memtable> memtable_list::new_memtable() {
     return make_lw_shared<memtable>(_current_schema(), *_dirty_memory_manager,
             _table_shared_data,
-            _table_stats, this, _compaction_scheduling_group);
+            _table_stats, this, _compaction_scheduling_group, _shared_gc_state);
 }
 
 // Synchronously swaps the active memtable with a new, empty one,