3ced5002b0fe5cfff71cc04c26f509729ff7cc11
Botond Dénes <bdenes@scylladb.com>
2025-09-12 13:49:36 +0300

    tools/webshell: introduce webshell skeleton
    Introduce the skeleton of webshell: the controller, http servers,
    configuration with just the bare-bones minimum to store future
    depdencies and implement the controller methods.
    
    The configuration already contains all items needed in the future.
    
    Also hook up to main, although the server doesn't do anything when
    started yet.

--- main.cc
+++ main.cc
@@ -138,6 +138,7 @@
 #include "utils/labels.hh"
 #include "tools/utils.hh"
 #include "schema/compression_initializer.hh"
+#include "tools/webshell/webshell.hh"
 
 
 namespace fs = std::filesystem;
@@ -2786,6 +2787,8 @@ To start the scylla server proper, simply invoke as: scylla server (or just scyl
 
             alternator::controller alternator_ctl(gossiper, proxy, ss, mm, sys_dist_ks, sys_ks, cdc_generation_service, service_memory_limiter, auth_service, sl_controller, vector_store_client, timeout_cfg, *cfg, dbcfg.statement_scheduling_group);
 
+            tools::webshell::controller webshell_ctl(qp, auth_service, sl_controller, tools::webshell::make_config(*cfg, cluster_name, dbcfg.statement_scheduling_group));
+
             // Register at_exit last, so that storage_service::drain_on_shutdown will be called first
             auto do_drain = defer_verbose_shutdown("local storage", [&ss] {
                 ss.local().drain_on_shutdown().get();
@@ -2812,6 +2815,10 @@ To start the scylla server proper, simply invoke as: scylla server (or just scyl
 
             if (bool enabled = cfg->alternator_port() || cfg->alternator_https_port()) {
                 ss.local().register_protocol_server(alternator_ctl, enabled).get();
+            }
+
+            if (cfg->webshell_http_port() || cfg->webshell_https_port()) {
+                ss.local().register_protocol_server(webshell_ctl, true).get();
             }
 
             stop_signal.ready();
