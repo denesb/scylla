6d2585187e70f93aa6deb6f13d07baa21793e9e4
Botond Dénes <bdenes@scylladb.com>
2026-09-02 12:05:31 +0300

    reader_concurrency_semaphore: enable the disk queue

--- reader_concurrency_semaphore.hh
+++ reader_concurrency_semaphore.hh
@@ -324,7 +324,7 @@ private:
     [[nodiscard]] mutation_reader detach_inactive_reader(reader_permit::impl&, evict_reason reason) noexcept;
     void evict(reader_permit::impl&, evict_reason reason) noexcept;
 
-    enum class reason { all_ok = 0, ready_list, need_cpu_permits, memory_resources, count_resources };
+    enum class reason { all_ok = 0, ready_list, need_cpu_permits, memory_resources };
     reason has_available_units(const resources& r) const;
 
     bool cpu_concurrency_limit_reached() const;
